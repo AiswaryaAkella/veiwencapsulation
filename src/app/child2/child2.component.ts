fe730cf3e24da4276256f7bcdd22e47ab17f6742
Aiswarya Akella <aiswaryaakella9@gmail.com>
2023-05-12 20:15:25 -0500

    Commit 1

---
+++ src/app/child2/child2.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-child2',
+  templateUrl: './child2.component.html',
+  styleUrls: ['./child2.component.css'],
+  encapsulation:ViewEncapsulation.ShadowDom
+})
+export class Child2Component {
+
+}
